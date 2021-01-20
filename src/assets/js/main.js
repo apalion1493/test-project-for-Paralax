f5965c2007c1b5403c7e3b9f8e116d2c640585d1
Alexandr Petrovich <apalion97@gmail.com>
2021-01-20 21:49:00 +0200

    first commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,6 @@
+import './config/jqueryLoad';
+import 'bootstrap/dist/js/bootstrap.min';
+import 'popper.js/dist/popper.min';
+
+$('[data-toggle="popover"]').popover();
+$('[data-toggle="tooltip"]').tooltip();
